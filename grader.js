47449e766d67a94069af0fae540f1a0e254be903
U-jefe-pc\jefe <jefe@jefe-pc.(none)>
2013-07-16 21:40:19 -0700

    removed console.log

--- grader.js
+++ grader.js
@@ -43,7 +43,7 @@ var assertFileExists = function(infile) {
 var assertUrlExists = function(inurl){
     var instr = inurl.toString();
     rest.get(instr).on('complete', function(result){
-        console.log(result);
+        //console.log(result);
     });
     
 };
